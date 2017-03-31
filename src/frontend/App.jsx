12724304c94d556e220ac30c55828a5ae7adffc4
D Mison <dmison@redhat.com>
2017-03-31 18:21:24 +1000

    using React Bootstrap

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Link, IndexRoute, Route, HashRouter} from 'react-router-dom';
+import {NavLink, Link, IndexRoute, Route, HashRouter} from 'react-router-dom';
 import {ApolloClient, ApolloProvider, createNetworkInterface} from 'react-apollo';
+
+import Nav from 'react-bootstrap/lib/Nav';
+import Navbar from 'react-bootstrap/lib/Navbar';
 
 import Home from './Home.jsx';
 import SessionsContainer from './SessionsContainer.js';
@@ -22,23 +25,36 @@ const App = (props) => {
   return (
     <ApolloProvider client={client}>
       <HashRouter>
-
     <div>
-      <h1>Summit @ Sites</h1>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/sessions'>Sessions</Link></li>
-        <li><Link to='/admin'>Admin</Link></li>
-        <li><a href='/logout'>logout</a></li>
-      </ul>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/sessions' component={SessionsContainer} />
-          <Route exact path='/admin' component={AdminContainer} />
+
+      <Navbar staticTop fluid inverse collapseOnSelect>
+        <div className='container'>
+        <Navbar.Header>
+          <Navbar.Brand>Summit @ Sites</Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href='#/'>Home</NavItem>
+            <NavItem eventKey={2} href='#/sessions'>Sessions</NavItem>
+            <NavItem eventKey={3} href='#/admin'>Admin</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={4} href='/logout'>Logout</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+      </Navbar>
+
+      <div className='container'>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/sessions' component={SessionsContainer} />
+        <Route exact path='/admin' component={AdminContainer} />
+      </div>
     </div>
   </HashRouter>
     </ApolloProvider>
   );
 }
-
 
 export default App;
